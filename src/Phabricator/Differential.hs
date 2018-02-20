ad44c5bc4c6696e185621dcd70d1b694724c0d1c
Ian-Woo Kim <ianwookim@gmail.com>
2018-02-20 15:08:40 +0000

    Lensified data type and introduce ItemType
    Summary: I lensified Phabricator.Differential data types.
    
    Test Plan: none
    
    Reviewers: wavewave
    
    Reviewed By: wavewave
    
    Subscribers: wavewave
    
    Maniphest Tasks: T630
    
    Differential Revision: https://uphere.phacility.com/D2218

--- src/Phabricator/Differential.hs
+++ src/Phabricator/Differential.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Phabricator.Differential where
 
-import           Control.Lens          ((^.))
+import           Control.Lens          ((^.),makeLenses)
 import           Data.Aeson
 import           Data.Aeson.Types
 import           Data.Maybe            (fromMaybe)
@@ -25,6 +26,8 @@ data Field = Field { _field_summary :: Text
                    }
            deriving (Show,Eq,Generic)
 
+makeLenses ''Field
+
 instance FromJSON Field where
   parseJSON = genericParseJSON (defaultOptions { fieldLabelModifier = drop 7 })
 
@@ -32,13 +35,34 @@ instance ToJSON Field where
   toJSON = genericToJSON (defaultOptions { fieldLabelModifier = drop 7 })
 
 
+data ItemType = DREV
+              deriving (Show,Eq)
+
+itemTypeToText :: ItemType -> Text
+itemTypeToText DREV = "DREV"
+
+textToItemType :: (Monad m) => Text -> m ItemType
+textToItemType "DREV" = pure DREV
+textToItemType x      = fail (T.unpack x  ++ " is not ItemType.")
+
+
+instance ToJSON ItemType where
+  toJSON = String . itemTypeToText
+
+instance FromJSON ItemType where
+  parseJSON (String txt) = textToItemType txt
+  parseJSON invalid = typeMismatch "ItemType" invalid
+
+
 data Item = Item { _item_attachments :: Value
                  , _item_phid :: Text
                  , _item_id :: Int
-                 , _item_type :: Text
+                 , _item_type :: ItemType
                  , _item_fields :: Field
                  }
               deriving (Show,Eq,Generic)
+
+makeLenses ''Item
 
 instance FromJSON Item where
   parseJSON = genericParseJSON (defaultOptions { fieldLabelModifier = drop 6 })
@@ -54,6 +78,7 @@ data QueryKey = Open
               | All
               deriving (Show,Eq,Generic)
 
+makeLenses ''QueryKey
 
 queryKeyToText :: QueryKey -> Text
 queryKeyToText Open     = "lks1dJdapQFa"
@@ -89,6 +114,7 @@ data PhabQuery = PhabQuery { _pq_queryKey :: QueryKey
                            }
                deriving (Show,Eq,Generic)
 
+makeLenses ''PhabQuery
 
 instance FromJSON PhabQuery where
   parseJSON = genericParseJSON (defaultOptions { fieldLabelModifier = drop 4 })
@@ -129,6 +155,8 @@ data PhabResult = PhabResult { _pr_maps :: Value
                              }
                 deriving (Show,Eq,Generic)
 
+makeLenses ''PhabResult
+
 instance FromJSON PhabResult where
   parseJSON = genericParseJSON (defaultOptions { fieldLabelModifier = drop 4 })
 
@@ -138,16 +166,20 @@ instance ToJSON PhabResult where
 
 
 
-data PhabResponse = PhabResponse { result :: PhabResult
-                                 , error_code :: Maybe Int
-                                 , error_info :: Maybe Text
+data PhabResponse = PhabResponse { _pres_result :: PhabResult
+                                 , _pres_error_code :: Maybe Int
+                                 , _pres_error_info :: Maybe Text
                                  }
                   deriving (Show,Eq,Generic)
 
+makeLenses ''PhabResponse
 
-instance FromJSON PhabResponse
+instance FromJSON PhabResponse where
+  parseJSON = genericParseJSON (defaultOptions { fieldLabelModifier = drop 6 })
 
-instance ToJSON PhabResponse
+
+instance ToJSON PhabResponse where
+  toJSON = genericToJSON (defaultOptions { fieldLabelModifier = drop 6})
 
 
 type URL = Text
@@ -162,4 +194,3 @@ runQuery url token query = do
              -- , "limit" := (5 :: Int)
              ])
   return (eitherDecode (r ^. responseBody) :: Either String PhabResponse)
-  -- print mv
